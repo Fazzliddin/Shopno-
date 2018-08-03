0feafe8cb78711f4c1abbf8b260f4fd31de01a10
Fazliddin <fazliddin-mirzaev99@bk.ru>
2018-08-03 12:19:12 +0500

    that is all

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,19 @@
 window.onload = function() {
 	var btnOpen = document.querySelector('.header__burger');
+	var burgerRotate = document.querySelector('.burger__rotate');
 	var btnClose = document.querySelector('.header__burger-close');
 	var menu = document.querySelector('.header__nav');
 
+
 	btnOpen.onclick = function() {
-		// alert(1);
 		menu.classList.toggle('show');
-		// anim.classList.toggle('anim');
 	};
 
 	btnClose.onclick = function() {
-		// alert(1);
 		menu.classList.toggle('show');
-		// anim.classList.toggle('anim');
 	};
 
+	
 	//parallax
 	
 };
